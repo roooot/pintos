beef911002bad60be8acf7a879587c50e0e24d68
Jinhwan Lee <jinhwan.lee@me.com>
2016-03-29 23:02:12 +0900

    prioirty scheduler

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -138,6 +138,8 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
+bool cmp_thread_priority (const struct list_elem *, 
+                          const struct list_elem *, void *);
 bool cmp_thread_wakeup (const struct list_elem *, 
                         const struct list_elem *, void *);
 
